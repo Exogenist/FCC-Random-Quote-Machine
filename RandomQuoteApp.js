271fca2dbe7439e3a3d8f5f9f805926e1ae067e6
Swainson Holness <swainson.holness@gmail.com>
2017-07-27 23:31:51 -0600

    Update RandomQuoteApp.js

--- RandomQuoteApp.js
+++ RandomQuoteApp.js
@@ -1,2 +1,8 @@
 var dataPackage = {};
 
+//Define random generator
+function getRandomIntInclusive(min, max) {
+    var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNum;
+}
+
