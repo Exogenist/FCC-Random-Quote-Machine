e697bb1c7b569f97a8ee37c7fb3fa1b48462c359
Swainson Holness <swainson.holness@gmail.com>
2017-07-27 23:32:21 -0600

    Update RandomQuoteApp.js

--- RandomQuoteApp.js
+++ RandomQuoteApp.js
@@ -6,3 +6,18 @@ function getRandomIntInclusive(min, max) {
     return randomNum;
 }
 
+//JQUERY SECTION
+$(document).ready(function() {    
+    //make api request and store response in object dataPackage 
+    $.getJSON("http://quotes.stormconsultancy.co.uk/quotes.json", function(data){
+        dataPackage["apiCall"] = data; 
+    });
+    
+    //Posts random quote on click event
+    $("#getMessage").on("click", function(){
+        var ranNum = getRandomIntInclusive(0,43);
+        $("#postQuote").text(dataPackage.apiCall[ranNum].quote);
+        $("#author").text("Author - " + dataPackage.apiCall[ranNum].author);
+    });   
+});
+
